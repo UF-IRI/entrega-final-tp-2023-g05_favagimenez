463aff753bcc587c28ce8050df38c285ec9ffa61
larafava <larafava13@gmail.com>
2023-11-21 20:12:03 -0300

    ver

--- Proyecto/main/main.cpp
+++ Proyecto/main/main.cpp
@@ -1,48 +1,23 @@
 #include<iostream>
 #include<fstream>
+#include "funciones.h"
 using namespace std;
-struct Student {
-    int roll_no;
-    string name;
-};
+
 int main() {
-    ofstream wf("student.dat", ios::out | ios::binary);
-    if(!wf) {
-        cout << "Cannot open file!" << endl;
-        return 1;
-    }
-    Student wstu[3];
-    wstu[0].roll_no = 1;
-    wstu[0].name = "Ram";
-    wstu[1].roll_no = 2;
-    wstu[1].name = "Shyam";
-    wstu[2].roll_no = 3;
-    wstu[2].name = "Madhu";
-    for(int i = 0; i < 3; i++)
-        wf.write((char *) &wstu[i], sizeof(Student));
-    wf.close();
-    if(!wf.good()) {
-        cout << "Error occurred at writing time!" << endl;
-        return 1;
-    }
-    ifstream rf("asistencias.dat", ios::out | ios::binary);
-    if(!rf) {
-        cout << "Cannot open file!" << endl;
-        return 1;
-    }
-    Student rstu[3];
-    for(int i = 0; i < 3; i++)
-        rf.read((char *) &rstu[i], sizeof(Student));
-    rf.close();
-    if(!rf.good()) {
-        cout << "Error occurred at reading time!" << endl;
-        return 1;
-    }
-    cout<<"Student's Details:"<<endl;
-    for(int i=0; i < 3; i++) {
-        cout << "Roll No: " << wstu[i].roll_no << endl;
-        cout << "Name: " << wstu[i].name << endl;
-        cout << endl;
-    }
+    cout<<"Hola munda1";
+    string rutaArchivoClases = "../Proyecto/iriClasesGYM.csv";
+    string rutaArchivoClientes = "../Proyecto/iriClientesGYM.csv";
+    string rutaArchivoAsistencias = "../Proyecto/asistencias.dat";
+   leerAsistencias(rutaArchivoAsistencias);
+    /*int cantAsistencias;
+    sAsistencia* asistencias = leerArchivoBinario("asistencias_1697673600000.dat", &cantAsistencias);
+    // "../gngng/
+    int cantidadClientes=0;
+    @@ -36,12 +71,41 @@ int main() {
+  //  Clases* listaClases = cargarClases("iriClasesGYM", cantidadClases);
+    //delete[] listaClases;*/
+
+    cout << "Hola mundo!"<<endl;
     return 0;
 }
+
